7020a82d721702cdf0bb5e62220832e191068fc2
lynop81 <lyndon_1947@outlook.com>
2026-06-28 19:30:26 -0400

    added win and lose screen

---
+++ ashley-ware/Scenes/done_screen.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimatedSprite2D.play("default")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
